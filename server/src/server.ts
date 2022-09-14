a02279e98441e904061c77f17da0231f60cedac7
Dants0 <gui.dg1@hotmail.com>
2022-09-14 19:22:00 -0300

    add: gitignore

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,16 +7,25 @@ app.get('/games', (req, res)=>{
 })
 
 app.post('/ads', (req, res)=>{
-    return res.json([]);
+    return res.status(201).json([]);
 })
 
-app.get('/ads', (req, res) =>{
-    res.json([
+app.get('/games/:id/ads', (req, res) =>{
+    // const gamesId = req.params.id;
+
+
+    return res.json([
         {id: 1, name: 'Anuncio_1'},
         {id: 2, name: 'Anuncio_2'},
         {id: 3, name: 'Anuncio_3'},
         {id: 4, name: 'Anuncio_4'},
     ])
+})
+
+app.get('/ads/:id/discord', (req, res) =>{
+    // const adId = req.params.id;
+
+    return res.json([])
 })
 
 app.listen(8080, ()=>{console.log('listening on port 8080')})
