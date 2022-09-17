1b963efcb9a4ed99a98ff1a8b19367d681145cd6
Dants0 <gui.dg1@hotmail.com>
2022-09-16 22:36:16 -0300

    fixes: add hover effect on cards ad

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -4,12 +4,10 @@ interface GameBannerProps{
     adsCount: number;
 }
 
-import React from 'react'
-
 const GameBanner = (props: GameBannerProps) => {
     return (
         <a href="" className='relative rounded-lg overflow-hidden'>
-            <img src={props.bannerUrl} alt="" />
+            <img src={props.bannerUrl} alt="" className="hover:scale-110 transition duration-300 ease-in-out"/>
             <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
                 <strong className="font-bold text-white block">{props.titleGame}</strong>
                 <span className="text-zinc-300 text-sm block">{props.adsCount} anúncio(s)</span>
